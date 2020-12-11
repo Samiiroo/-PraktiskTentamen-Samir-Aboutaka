367b66626c9039283323f25f060fb2ea0bbffb93
Samir Aboutaka <43029436+Samiiroo@users.noreply.github.com>
2020-12-11 12:27:45 +0100

    klart

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 /* Skriv din kod här */
 const URL = 'https://restcountries.eu/rest/v2/all';
 
-
+//hämtar responsen och kollar om det finns errorhär
 fetch(URL).then(
     function(response){
         if(response.status === 404){
             throw 'Not found';
         }
-        else{
-            return response.json();
-        }
+            if (response.status === 100) {
+                throw 'The API key passed was not valid or has expired';
+            } else if (response.status === 105) {
+                throw 'The requested service is temporarily unavailable'
+            } else if (response.status === 106) {
+                throw 'The requested operation failed due to a temporary issue.'
+            } else if (response.status === 111) {
+                throw 'The requested response format was not found'
+            } else if (response.status === 112) {
+                throw 'The requested method was not found'
+            } else if (response.status === 114) {
+                throw 'The SOAP envelope send in the request could not be parsed.'
+            } else if (response.status === 115) {
+                throw 'The XML-RPC request document could not be parsed.'
+            }else if (response.status >= 200 && response.status <= 299) { //finns det ingen error så ska det forsätta som den ska alltså returna respons.json
+                return response.json();
+            }
+    
     
     }
 ).then(
     function(data){
-        let countries = []
-        for (let i = 0; i < 3; i++) {
-            let random = Math.floor(Math.random() * data.length)
-            let country =new Country(data[random].flag, data[random].name, data[random].timezones[0]);
+        let countries = [] //skapar tom array som ska innnehåla länderna
+        for (let i = 0; i < 3; i++) { // loop som loopar igenom 3 slumpade länder
+            let random = Math.floor(Math.random() * data.length) // random funktkionen
+            let country =new Country(data[random].flag, data[random].name, data[random].timezones[0]); // contstruktorn där vi hämtar flagga namn och tid med hjälp av constructorn
             countries.push(country);
 
         }
-            let img = document.querySelectorAll('img');
+        /// hämtar img name och time från HTML DOM Och sedan ger jag dem 3 länderna och lägger in dem i innerhtml med hjälp av DOM och tiden med hjälåp av time prototyp
+            let img = document.querySelectorAll('img'); 
             let name = document.querySelectorAll('h1');
             let time = document.querySelectorAll('h3');
            
@@ -34,7 +50,7 @@ fetch(URL).then(
         })
         
             
-
+///constructor
 function Country(_img,_name,_timezone){
     this.img = _img;
     this.name = _name;
